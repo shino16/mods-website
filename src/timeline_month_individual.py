3ad90b961064bc6050e2a4b1ad2a899a552ad80f
shino-sky <ms_020916@yahoo.co.jp>
2020-04-17 22:14:44 +0900

    display scores for each problem on contest page

--- src/timeline_month_individual.py
+++ src/timeline_month_individual.py
@@ -34,11 +34,16 @@ def run(month):
     tablehtml = ""
     if month in s_db_y:
         for row in s_db_y[month]:
-            rowhtml = templates.get("timeline/month/individual_row")
+            rowhtml = templates.get("timeline/month/individual_row_" + str(len(row["scores"])))
             rowhtml = rowhtml.replace("__NAME__", row["name"])
             rowhtml = rowhtml.replace("__USER_ID__", row["user-id"])
+            rowhtml = rowhtml.replace("__TOTAL_SCORE__", str(row["total_score"]))
             rowhtml = rowhtml.replace("__RANK__", str(row["rank"]))
-            rowhtml = rowhtml.replace("__SCORE__", str(row["total_score"]))
+            rowhtml = rowhtml.replace("__TABLE_HEADER__", str())
+
+            for i, x in enumerate(row["scores"]):
+                rowhtml = rowhtml.replace(f"__SCORE_{i+1}__", str(x))
+
             if row["medal"] == "G":
                 rowhtml = rowhtml.replace("__MEDAL__", templates.get("timeline/month/individual_gold"))
             elif row["medal"] == "S":
@@ -51,6 +56,11 @@ def run(month):
                 rowhtml = rowhtml.replace("__MEDAL__", "")
             tablehtml += rowhtml
     html = html.replace("__TABLE__", tablehtml)
+
+    header = ""
+    if month in s_db_y and len(s_db_y[month]) >= 1:
+        header = templates.get("timeline/month/individual_header_" + str(len(s_db_y[month][0]["scores"])))
+    html = html.replace("__TABLE_HEADER__", header)
 
     html = templates.final_replace(html, "../..")
     util.writefile("../dist/timeline/" + month + "/individual.html", html)
