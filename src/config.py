d5e3241b3adfdcdd3159ad09639b69264dfd6c4c
shino-sky <ms_020916@yahoo.co.jp>
2020-04-18 20:42:40 +0900

    total results includes all

--- src/config.py
+++ src/config.py
@@ -3,8 +3,8 @@
 email = "mo.discord.server@gmail.com"
 
 # Technically, these can be non-consecutive
-previous_month = "Jan.2020"
-next_month = "Feb.2020"
+previous_month = "2020-02"
+next_month = "2020-03"
 
 # On github, the links don't have to have .html prefix,
 #   and directories forward to index.html inside.
