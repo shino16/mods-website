27a064c4c652201bde9595e3bc1adf8e1bffd534
shino-sky <ms_020916@yahoo.co.jp>
2020-06-03 16:00:48 +0900

    generate database csv

--- src/static_files.py
+++ src/static_files.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 from distutils.dir_util import copy_tree
+from database_students import database as s_db
+from database_timeline import database as t_db
 
 def run():
     print("Copying static files")
     copy_tree("./templates/img",  "../dest/img")
     copy_tree("./templates/css",  "../dest/css")
+    with open("../dest/search/students.csv", "w", encoding="utf8") as f:
+        for entry in s_db:
+            f.write(",".join(map(str, entry.values())))
+            f.write("\n")
+    with open("../dest/search/timeline.csv", "w", encoding="utf8") as f:
+        for entry in t_db:
+            f.write(",".join(map(str, entry.values())))
+            f.write("\n")
 
 if __name__ == "__main__":
     run()
