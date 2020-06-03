7bb66b6c2d1d7885567fe38276abf5e6d12dc7e9
shino-sky <ms_020916@yahoo.co.jp>
2020-06-03 13:01:23 +0900

    update header

--- src/config.py
+++ src/config.py
@@ -3,8 +3,8 @@
 email = "mo.discord.server@gmail.com"
 
 # Technically, these can be non-consecutive
-previous_month = "2020-03"
-next_month = "2020-04"
+previous_month = "2020-04"
+next_month = "2020-05"
 
 # On github, the links don't have to have .html prefix,
 #   and directories forward to index.html inside.
